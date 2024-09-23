d7d90ebf139f934fecc00556df45c1df071d7ffe
Augusto <augustoortolan02@gmail.com>
2024-09-23 19:54:40 -0300

    scrollview

--- App.js
+++ App.js
@@ -1,20 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+
+const pessoas = [
+  {
+    nome: "Augusto",
+    sobrenome: "Ortolan",
+  },
+  {
+    nome: "Luiz",
+    sobrenome: "Pereira",
+  },
+  {
+    nome: "Ana",
+    sobrenome: "Santos",
+  },
+  {
+    nome: "Laura",
+    sobrenome: "Ramos",
+  },
+  {
+    nome: "Otavio",
+    sobrenome: "Santos",
+  },
+  {
+    nome: "Luiz",
+    sobrenome: "Pereira",
+  },
+  {
+    nome: "Ana",
+    sobrenome: "Santos",
+  },
+  {
+    nome: "Laura",
+    sobrenome: "Ramos",
+  },
+  {
+    nome: "Otavio",
+    sobrenome: "Santos",
+  },
+  {
+    nome: "Augusto",
+    sobrenome: "Ortolan",
+  },
+  {
+    nome: "Luiz",
+    sobrenome: "Pereira",
+  },
+  {
+    nome: "Ana",
+    sobrenome: "Santos",
+  },
+  {
+    nome: "Laura",
+    sobrenome: "Ramos",
+  },
+  {
+    nome: "Otavio",
+    sobrenome: "Santos",
+  },
+  {
+    nome: "Luiz",
+    sobrenome: "Pereira",
+  },
+  {
+    nome: "Ana",
+    sobrenome: "Santos",
+  },
+  {
+    nome: "Laura",
+    sobrenome: "Ramos",
+  },
+  {
+    nome: "Otavio",
+    sobrenome: "Santos",
+  },
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      {pessoas.map((pessoa) => (
+        <View style={styles.containerPessoa}>
+          <Text style={styles.textoPessoa}>{pessoa.nome}</Text>
+          <Text style={styles.textoPessoa}>{pessoa.sobrenome}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  containerPessoa: {
+    borderWidth: 1,
+    marginBottom: 10,
+    width: "100%",
+    height: 100,
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+  textoPessoa: {
+    fontSize: 20,
   },
 });
